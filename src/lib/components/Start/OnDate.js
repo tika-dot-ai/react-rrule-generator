9735f7c6d3191484f641fe7cc4b28b5b55d04fbb
ValeriiMal <valmal24.work@gmail.com>
2019-05-23 22:41:33 +0300

    add react-day-picker for date select

--- src/lib/components/Start/OnDate.js
+++ src/lib/components/Start/OnDate.js
@@ -4,6 +4,9 @@ import moment from 'moment';
 import DateTime from 'react-datetime';
 import 'moment/min/locales';
 
+import DayPicker from 'react-day-picker';
+import { Popup, Button } from 'semantic-ui-react';
+
 import { DATE_TIME_FORMAT } from '../../constants/index';
 import translateLabel from '../../utils/translateLabel';
 
@@ -16,63 +19,99 @@ const StartOnDate = ({
   handleChange,
   translations
 }) => {
-  const CustomCalendar = options.calendarComponent;
+  const [selectedDay, setSelectedDay] = React.useState(moment(date).toDate());
+
+  // const CustomCalendar = options.calendarComponent;
   const locale = options.weekStartsOnSunday ? 'en-ca' : 'en-gb';
-  const calendarAttributes = {
+  // const calendarAttributes = {
+  //   'aria-label': translateLabel(translations, 'start.tooltip'),
+  //   value: date,
+  //   dateFormat: DATE_TIME_FORMAT,
+  //   locale,
+  //   readOnly: true,
+  // };
+  const dayPickerAttributes = {
     'aria-label': translateLabel(translations, 'start.tooltip'),
-    value: date,
-    dateFormat: DATE_TIME_FORMAT,
     locale,
-    readOnly: true,
+  };
+
+  const handleDayClick = (day) => {
+    setSelectedDay(day);
+    const editedEvent = {
+      target: {
+        value: moment(day).format(DATE_TIME_FORMAT),
+        name: 'start.onDate.date',
+      },
+    };
+    handleChange(editedEvent);
   };
 
   return (
-    <div className="col-6 col-sm-3">
-      {
-        CustomCalendar
-          ? <CustomCalendar
-            key={`${id}-calendar`}
-            {...calendarAttributes}
-            onChange={(event) => {
-              const editedEvent = {
-                target: {
-                  value: event.target.value,
-                  name: 'start.onDate.date',
-                },
-              };
-
-              handleChange(editedEvent);
-            }}
-          />
-          : <DateTime
-            {...calendarAttributes}
-            inputProps={
-              {
-                id: `${id}-datetime`,
-                name: 'start.onDate.date',
-                readOnly: true,
-              }
-            }
-            locale={translateLabel(translations, 'locale')}
-            timeFormat={false}
-            viewMode="days"
-            closeOnSelect
-            closeOnTab
-            required
-            onChange={(inputDate) => {
-              const editedEvent = {
-                target: {
-                  value: moment(inputDate).format(DATE_TIME_FORMAT),
-                  name: 'start.onDate.date',
-                },
-              };
-
-              handleChange(editedEvent);
-            }}
-          />
-      }
-    </div>
+    <Popup
+      trigger={(
+        <Button>
+          {date}
+        </Button>
+      )}
+      content={(
+        <DayPicker
+          selectedDays={selectedDay}
+          onDayClick={handleDayClick}
+          {...dayPickerAttributes}
+          month={moment(date).toDate()}
+        />
+      )}
+      on="click"
+    />
   );
+  
+  // return (
+  //   <div className="col-6 col-sm-3">
+  //     {
+  //       CustomCalendar
+  //         ? <CustomCalendar
+  //           key={`${id}-calendar`}
+  //           {...calendarAttributes}
+  //           onChange={(event) => {
+  //             const editedEvent = {
+  //               target: {
+  //                 value: event.target.value,
+  //                 name: 'start.onDate.date',
+  //               },
+  //             };
+
+  //             handleChange(editedEvent);
+  //           }}
+  //         />
+  //         : <DateTime
+  //           {...calendarAttributes}
+  //           inputProps={
+  //             {
+  //               id: `${id}-datetime`,
+  //               name: 'start.onDate.date',
+  //               readOnly: true,
+  //             }
+  //           }
+  //           locale={translateLabel(translations, 'locale')}
+  //           timeFormat={false}
+  //           viewMode="days"
+  //           closeOnSelect
+  //           closeOnTab
+  //           required
+  //           onChange={(inputDate) => {
+  //             const editedEvent = {
+  //               target: {
+  //                 value: moment(inputDate).format(DATE_TIME_FORMAT),
+  //                 name: 'start.onDate.date',
+  //               },
+  //             };
+
+  //             handleChange(editedEvent);
+  //           }}
+  //         />
+  //     }
+  //   </div>
+  // );
 };
 
 StartOnDate.propTypes = {
